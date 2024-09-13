62ba615987ee2959e35a9f7115f5ef76cb3bdbc4
gopika-gopal <140744560+gopika-gopal@users.noreply.github.com>
2024-09-13 13:49:05 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Taiyo.AI_Frontend_Assessment/"
+  // base:"/Taiyo.AI_Frontend_Assessment/"
 })
